66dc81219d8a8bc84957567a1eca41801f523088
Niklas Terhoeven <niklas.terhoeven@uni-wuerzburg.de>
2017-10-23 10:29:16 +0200

    add blast commands

--- scripts/05_Classifier.sh
+++ scripts/05_Classifier.sh
@@ -2,12 +2,15 @@
 
 source reper.conf
 ulimit -v "$maxMemoryKB"
-#blast vs database
 
-#filter vs chloro and mito
+#blast vs database
+"$blastn" -query "$CLUSTEROUT" -db "$classificationDB" -outfmt 7 -out "$CLUSTEROUT"_class.bls -num_threads "MAXTHREADS" -task 'blastn'
+"$blastn" -query "$CLUSTEROUT" -db "$chloroDB" -outfmt 7 -out "$CLUSTEROUT"_chloro.bls -num_threads "MAXTHREADS" -task 'blastn'
+"$blastn" -query "$CLUSTEROUT" -db "$mitoDB" -outfmt 7 -out "$CLUSTEROUT"_mito.bls -num_threads "MAXTHREADS" -task 'blastn'
+
+#analyse blast results
 
 
 echo "###################################"
 echo "finished classification -running quantification"
-echo "running filtering against chloroplast and mitochondrion"
 "$reperDir"/reper quantify
