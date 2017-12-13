f58a872d6fbb2ba7103c674cbeb50602165db9cb
Niklas Terhoeven <niklas.terhoeven@uni-wuerzburg.de>
2017-12-13 09:56:36 +0100

    fix

--- scripts/04_cd-hit.sh
+++ scripts/04_cd-hit.sh
@@ -2,7 +2,7 @@
 
 source ./reper.conf
 
-$maxMemoryMB=$(echo "$maxMemoryKB/1000" | bc)
+maxMemoryMB=$(echo "$maxMemoryKB/1000" | bc)
 #cluster sequences with identity>0.8 and length>0.9; 32 CPUs 10GB Memory limit; -d 0 disables the sequence name cutoff
 
 "$cdHit" -i "$TRINITYOUT" -o "$CLUSTEROUT" -d 0 -c 0.8 -n 5 -G 0 -aS 0.9 -T "$MAXTHREADS" -M "$maxMemoryMB"
